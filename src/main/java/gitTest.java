d19457356e93e5fc77b2da24b4fe674758d341a8
JavaES6 <85978305+JavaES6@users.noreply.github.com>
2021-10-21 08:54:25 +0800

    Update gitTest.java

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -5,5 +5,7 @@ public class gitTest {
         System.out.println("12345");
         System.out.println("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 ");
         System.out.println("Pull");
+                System.out.println("Push");
+
     }
 }
